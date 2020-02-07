d7acff9e1410f77a219f85d9e70520ff4f46a194
debanjan pal <sangeet_joy@yahoo.com>
2020-02-08 00:57:32 +0530

    Player class

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log('Ludo');
-console.log('luu')
+class Player {
+   
+    constructor(number) {
+        this.player = number;
+        this.numberOfCoin = 4;
+        this.playingCoin = 0;
+    }
+} 
+
+
+let p = new Player(1);
+
+console.log(p);
+
+class Ludo {
+
+}

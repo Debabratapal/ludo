7d3aedd8f1ab3d2894fcdcc7e8b825fc5d055af7
debanjan pal <sangeet_joy@yahoo.com>
2020-02-08 01:32:08 +0530

    ludo play

--- index.js
+++ index.js
@@ -4,14 +4,34 @@ class Player {
         this.player = number;
         this.numberOfCoin = 4;
         this.playingCoin = 0;
+        this.numberOfSixes = 0;
+        this.currentNumber = 0;
+    }
+
+    suffel () {
+       let number = Math.round(Math.random() * (6 - 1) + 1);
+       if(number === 6) {
+           this.numberOfSixes  = this.numberOfSixes + 1;
+       }
+       this.currentNumber = number;
     }
 } 
 
-
-let p = new Player(1);
-
-console.log(p);
-
 class Ludo {
+    constructor() {
+        this.players = [];
+        for(let i=0; i<4; i++) {
+            this.players.push(new Player(i+1));
+        }
+        this.currentPlayer = 1;
+    }
 
+    play() {
+        let player = this.players[this.currentPlayer -1];
+        player.suffel();
+    }
 }
+
+let ludo = new Ludo();
+ludo.play();
+console.log(ludo);
